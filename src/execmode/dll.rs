54093b6a8157b000915b1dbf85526d9f197ceb6f
Brother-x86 <heyler.quentin@gmail.com>
2024-12-21 09:21:18 +0100

    cleaning

--- src/execmode/dll.rs
+++ src/execmode/dll.rs
@@ -1,30 +1,20 @@
-#[cfg(target_os = "windows")]
 use windows::{Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK}, Win32::System::SystemServices::*,};
-#[cfg(target_os = "windows")]
 use windows::core::s;
+use crate::run_loader;
 
 //https://fluxsec.red/rust-dll-windows-api
-// cargo build --target x86_64-pc-windows-gnu --lib --release
-
-
-#[cfg(feature = "loader")]
-#[cfg(feature = "dll")]
-use crate::execmode::executable::run_loader;
-
+//TODO: https://blog.nviso.eu/2020/08/04/debugging-dlls-3-techniques-to-help-you-get-started/
 
 #[no_mangle]
 #[allow(non_snake_case)]
-#[cfg(target_os = "windows")]
 fn DllMain(_: usize, dw_reason: u32, _: usize) -> i32 {
     match dw_reason {
         DLL_PROCESS_ATTACH => attach(),
         _ => (),
     }
-
     1
 }
 
-#[cfg(target_os = "windows")]
 fn attach() {
     unsafe {
         MessageBoxA(None, s!("Hello from MALLEABLE"), s!("MALLEABLE"), MB_OK);
--- src/lib.rs
+++ src/lib.rs
@@ -13,66 +13,17 @@ pub mod python_embedder;
 
 // Module regroupant les fonctionnalités
 pub mod execmode {
-    //pub mod commune;
-
     #[cfg(feature = "loader")]
     pub mod executable;
-    //pub mod bibliotheque;
 
+    #[cfg(feature = "loader")]
+    #[cfg(feature = "dll")]
     pub mod dll;
 
 }
 
-// Rendre les fonctionnalités accessibles directement
-//pub use featexecmodeures::commune::fonctionnalite_commune;
 
 #[cfg(feature = "loader")]
 pub use execmode::executable::run_loader;
-//pub use execmode
 
 
-
-/*
-#[cfg(feature = "dll")]
-#[cfg(feature = "loader")]
-#[no_mangle]  // Empêche le compilateur de modifier le nom de la fonction (mangling)
-pub extern "C" fn DllInstall() {
-    println!("Running loader from DLL!");
-    run_loader()
-}
- */
-
-
- /*
-
-#[cfg(feature = "dll")]
-#[cfg(feature = "loader")]
-use windows::{Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK}, Win32::System::SystemServices::*,};
-
-#[cfg(feature = "dll")]
-#[cfg(feature = "loader")]
-use windows::core::s;
-
-#[cfg(feature = "dll")]
-#[cfg(feature = "loader")]
-#[no_mangle]
-#[allow(non_snake_case)]
-//pub extern "C" fn DllInstall() {
-fn DllInstall(_: usize, dw_reason: u32, _: usize) -> i32 {
-    match dw_reason {
-        DLL_PROCESS_ATTACH => attach(),
-        _ => (),
-    }
-
-    1
-}
-
-#[cfg(feature = "dll")]
-#[cfg(feature = "loader")]
-fn attach() {
-    unsafe {
-        MessageBoxA(None, s!("Hello from Rust DLL"), s!("Hello from Rust DLL"), MB_OK);
-    }
-}
-
-     */
