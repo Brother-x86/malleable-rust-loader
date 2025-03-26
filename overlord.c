fa2b2a87eb19c4fe7d6986690cbfcce778f3c55d
Brother-x86 <heyler.quentin@gmail.com>
2025-03-26 08:44:40 +0100

    overlord

--- overlord.c
+++ overlord.c
@@ -1,11 +1,13 @@
 #include <windows.h>
 #include <stdio.h>
-//#include <unistd.h>
+#include <unistd.h>
 
 // Déclare un type de fonction correspondant à `Overlord`
 typedef void (__stdcall *OverlordFunc)(void);
 
 int main() {
+    printf("LoadLibrary\n");
+    sleep(1);
     HMODULE dll_handle = LoadLibrary("REPLACEME");
     if (!dll_handle) {
         printf("Failed to load DLL\n");
@@ -13,6 +15,10 @@ int main() {
     }
     //printf("Le programme va dormir pendant 10 secondes...\n");
     //sleep(10);  // Met le programme en pause pendant 10 secondes
+    for (int i = 0; i < 10; i++) {
+        printf("sleep en c\n");
+        sleep(1); // Pause de 1 seconde
+    }
     // Obtenez l'adresse de la fonction 'Overlord'
     OverlordFunc Overlord = (OverlordFunc)GetProcAddress(dll_handle, "Overlord");
     if (!Overlord) {
