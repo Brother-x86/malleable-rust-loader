0c313f706391cee32a44c49862b2ac0d8c26062c
Brother-x86 <heyler.quentin@gmail.com>
2026-03-29 22:10:53 +0200

    ollvm return

--- winrust.py
+++ winrust.py
@@ -202,7 +202,84 @@ NOT ACTIVATED:
         #comm=f'''sudo docker run -v $(pwd):/projects/ -w /projects -e LITCRYPT_ENCRYPT_KEY="$LITCRYPT_ENCRYPT_KEY" -e CARGO_TARGET_DIR=ollvm -it ngtystr/rust-obfuscator-llvm:1.89.0-20.1.5-20251230 cargo rustc {compilation_args} --features ollvm --target x86_64-pc-windows-gnu --release'''
         #comm=f'''sudo docker run -v $(pwd):/projects/ -w /projects -e LITCRYPT_ENCRYPT_KEY="$LITCRYPT_ENCRYPT_KEY" -e CARGO_TARGET_DIR=ollvm -e RUSTFLAGS="-C llvm-args=--enable-bcfobf -C llvm-args=--enable-antihook -C llvm-args=--enable-allobf -C llvm-args=--enable-acdobf -C llvm-args=--enable-cffobf -C llvm-args=--enable-fco -C llvm-args=--enable-funcwra -C llvm-args=--enable-indibran -C llvm-args=--enable-name-compression -C llvm-args=--enable-splitobf -C llvm-args=--enable-subobf" -it ngtystr/rust-obfuscator-llvm:1.89.0-20.1.5-20251230 cargo rustc {compilation_args} --features ollvm --target x86_64-pc-windows-gnu --release -- -C llvm-args="--enable-bcfobf --enable-antihook --enable-allobf --enable-acdobf --enable-cffobf --enable-fco --enable-funcwra --enable-indibran --enable-name-compression --enable-splitobf --enable-subobf" '''
         # --enable-allobf activation de TOUT.
-        comm=f'''sudo docker run -v $(pwd):/projects/ -w /projects -e CARGO_TARGET_DIR=ollvm -e RUSTFLAGS="-C llvm-args=--enable-bcfobf -C llvm-args=--enable-antihook -C llvm-args=--enable-strcry -C llvm-args=--strcry_prob=100 -C llvm-args=--enable-constenc -C llvm-args=--enable-acdobf -C llvm-args=--enable-cffobf -C llvm-args=--enable-fco -C llvm-args=--enable-funcwra -C llvm-args=--enable-indibran -C llvm-args=--enable-name-compression -C llvm-args=--enable-splitobf -C llvm-args=--enable-subobf" -it ngtystr/rust-obfuscator-llvm:1.89.0-20.1.5-20251230 cargo rustc {compilation_args} --features ollvm --target x86_64-pc-windows-gnu --release'''
+        #comm=f'''sudo docker run -v $(pwd):/projects/ -w /projects -e CARGO_TARGET_DIR=ollvm -e RUSTFLAGS="-C llvm-args=--enable-bcfobf -C llvm-args=--enable-antihook -C llvm-args=--enable-strcry -C llvm-args=--strcry_prob=100 -C llvm-args=--enable-constenc -C llvm-args=--enable-acdobf -C llvm-args=--enable-cffobf -C llvm-args=--enable-fco -C llvm-args=--enable-funcwra -C llvm-args=--enable-indibran -C llvm-args=--enable-name-compression -C llvm-args=--enable-splitobf -C llvm-args=--enable-subobf" -it ngtystr/rust-obfuscator-llvm:1.89.0-20.1.5-20251230 cargo rustc {compilation_args} --features ollvm --target x86_64-pc-windows-gnu --release'''
+
+
+        rustflags = " ".join([
+            "-C llvm-args=--enable-bcfobf",
+            "-C llvm-args=--enable-antihook",
+            "-C llvm-args=--enable-strcry",
+            "-C llvm-args=--strcry_prob=100",
+            "-C llvm-args=--enable-constenc",
+            "-C llvm-args=--enable-acdobf",
+            "-C llvm-args=--enable-cffobf",
+            "-C llvm-args=--enable-fco",
+            "-C llvm-args=--enable-funcwra",
+            "-C llvm-args=--enable-indibran",
+            "-C llvm-args=--enable-name-compression",
+            "-C llvm-args=--enable-splitobf",
+            "-C llvm-args=--enable-subobf",
+        ])
+    # https://claude.ai/chat/7e82e06a-b6ee-4cca-8142-6fab827d78df
+    # Les flags les plus gourmands en RAM/CPU sont clairement bcf_loop, constenc_times et fw_times. Voici une config allégée :
+
+        rustflags = " ".join([
+            "-C llvm-args=--enable-bcfobf",
+            "-C llvm-args=--enable-antihook",
+            "-C llvm-args=--enable-strcry",
+            "-C llvm-args=--strcry_prob=100",
+            "-C llvm-args=--enable-constenc",
+            "-C llvm-args=--enable-acdobf",
+            "-C llvm-args=--enable-cffobf",
+            "-C llvm-args=--enable-fco",
+            "-C llvm-args=--enable-funcwra",
+            "-C llvm-args=--enable-indibran",
+            "-C llvm-args=--enable-name-compression",
+            "-C llvm-args=--enable-splitobf",
+            "-C llvm-args=--enable-subobf",
+            # BCF junk code
+            "-C llvm-args=--bcf_prob=50",
+            "-C llvm-args=--bcf_loop=1",
+            "-C llvm-args=--bcf_junkasm",
+            "-C llvm-args=--bcf_junkasm_minnum=1",
+            "-C llvm-args=--bcf_junkasm_maxnum=2",
+            # Substitution
+            "-C llvm-args=--sub_prob=100",
+            "-C llvm-args=--sub_loop=1",
+            # Constant encryption
+            "-C llvm-args=--constenc_times=1",
+            "-C llvm-args=--constenc_togv",
+            "-C llvm-args=--constenc_togv_prob=100",
+            "-C llvm-args=--constenc_subxor",
+            "-C llvm-args=--constenc_subxor_prob=100",
+            # Indirect branch
+            "-C llvm-args=--indibran-use-stack",
+            "-C llvm-args=--indibran-enc-jump-target",
+            # Function wrapper
+            "-C llvm-args=--fw_prob=100",
+            "-C llvm-args=--fw_times=1",
+            # Block splitting
+            "-C llvm-args=--split_num=1",
+            "-C llvm-args=--hikari",  # Active le moteur IR obfuscation global
+
+            "-C llvm-args=--bcf_createfunc",  # Met le junk dans de vraies fonctions = plus dur à éliminer
+            "-C link-arg=-Wl,--no-gc-sections",  # Désactive le garbage collect des sections
+        ])
+
+
+        comm = (
+            f"sudo docker run "
+            f"-v $(pwd):/projects/ "
+            f"-w /projects "
+            f"-e CARGO_TARGET_DIR=ollvm "
+            f'-e RUSTFLAGS="{rustflags}" '
+            f"-it ngtystr/rust-obfuscator-llvm:1.89.0-20.1.5-20251230 "
+            f"cargo rustc {compilation_args} "
+            f"--features ollvm "
+            f"--target x86_64-pc-windows-gnu "
+            f"--release"
+        )
+
         log.info(comm)
         compil_result=os.system(comm)
         # DEBUG probleme compilation
